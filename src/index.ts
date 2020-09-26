64f0550d253135a1995b072e222de9fe4be4b0d9
Gonzalo Rocha <grochadc@gmail.com>
2020-09-25 20:13:18 -0500

    HEROKU bind port

--- src/index.ts
+++ src/index.ts
@@ -62,6 +62,6 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 5000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
