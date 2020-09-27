543fe1354d9492c7d17759a007211871104f4991
Gonzalo Rocha <grochadc@gmail.com>
2020-09-27 02:39:44 -0500

    Separate typedefs and resolvers

---
+++ src/schema.ts
@@ -0,0 +1,44 @@
+import { gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Workshop {
+    name: String
+    description: String
+  }
+
+  type Option {
+    id: ID!
+    teacher: String
+    time: String
+    day: String
+    workshop: Workshop!
+  }
+
+  type Applicant {
+    id: ID!
+    code: String
+    name: String
+    option: Option!
+  }
+
+  type Teacher {
+    id: ID!
+    name: String
+    options: [Option]
+    reservations: [Applicant]
+    reservations_by_day(day: String): [Applicant]
+  }
+
+  type Query {
+    workshops: [Workshop]
+    workshop(id: ID!): Workshop
+    options: [Option]
+    option(id: ID!): Option
+    applicants: [Applicant]
+    applicant(id: ID!): Applicant
+    teachers: [Teacher]
+    teacher(id: ID!, name: String): Teacher
+  }
+`;
+
+export default typeDefs;
