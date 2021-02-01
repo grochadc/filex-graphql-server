94ff45071c87f455bf6dc8dbb1a6146f2119a1c9
Gonzalo <grochadc@gmail.com>
2021-02-01 12:15:51 -0600

    Test firebase

--- src/datasources/firebase.ts
+++ src/datasources/firebase.ts
@@ -17,16 +17,27 @@ class FirebaseAPI extends DataSource {
       ? firebase.initializeApp(clientConfig)
       : firebase.app();
     this.client = firebaseApp.database();
+    this.client
+      .ref("onlineUsers")
+      .once("value")
+      .then((snapshot) => console.log("value ", snapshot.val()))
+      .catch((e) => console.log("error ", e));
   }
   async get(ref: string) {
-    return await this.client
+    console.log(`Getting ref ${ref}`);
+    return this.client
       .ref(ref)
       .once("value")
-      .then((snapshot: { val: any }) => snapshot.val())
-      .catch(console.log);
+      .then((snapshot: { val: any }) => {
+        const data = snapshot.val();
+        console.log(`DataSource got value ${data}`);
+        return data;
+      })
+      .catch((e) => console.log(`Caught eror ${e}`));
   }
 
   async post(ref: string, data: any) {
+    console.log(`Posting data ${data} to ref ${ref}`);
     return await this.client.ref(ref).set(data);
   }
 }
