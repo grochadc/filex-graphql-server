033f2727684b2b827572ab44ea94c8a99d5d4452
Gonzalo <grochadc@gmail.com>
2021-02-10 15:43:40 -0600

    Remove inexistent datasources from datasources/index

--- src/datasources/index.ts
+++ src/datasources/index.ts
@@ -1,7 +1,5 @@
 import { FirebaseAPI } from "./myREST";
 import { RegistroAPI } from "./RegistroAPI";
-import { StudentsAPI } from "./StudentsAPI";
-import { WorkshopsAPI } from "./WorkshopsAPI";
 import { ExamAPI } from "./ExamAPI";
 
-export { RegistroAPI, FirebaseAPI, StudentsAPI, WorkshopsAPI, ExamAPI };
+export { RegistroAPI, FirebaseAPI, ExamAPI };
