a0614e186be78d18da66c0f7136568daf2d0d96e
Gonzalo Rocha <grochadc@gmail.com>
2021-03-01 22:06:41 -0600

    Expand cupo to 35 ss

--- src/modules/registro/index.ts
+++ src/modules/registro/index.ts
@@ -123,7 +123,7 @@ export const resolvers = {
     schedules: async (root, args, { dataSources }) => {
       const course = root.curso;
       const currentLevel = root.nivel;
-      const maxStudents = 30;
+      const maxStudents = 35;
       const unavailable = await dataSources.registroAPI.getUnAvailableGroups(
         currentLevel,
         maxStudents,
