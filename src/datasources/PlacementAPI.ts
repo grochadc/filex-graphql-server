30725a53d9a3142d4ca255eab60746822d38e1b1
Gonzalo Rocha <grochadc@gmail.com>
2021-08-14 15:28:55 -0500

    Fix links overwriting each other

--- src/datasources/PlacementAPI.ts
+++ src/datasources/PlacementAPI.ts
@@ -20,21 +20,26 @@ class PlacementAPI extends RESTDataSource {
     return result;
   }
 
-  async saveMeetLinks(links: { teacher: string; link: string }[]) {
-    return this.put(`/meetLinks.json`, links);
+  async saveMeetLinks(links: { teacher: string; link: string }[], env) {
+    const defaultLinksUrl =
+      env === "dev" ? `/${env}/meetLinks.json` : `/meetLinks.json`;
+    console.log("saving meet links in", defaultLinksUrl);
+    return this.put(defaultLinksUrl, links);
   }
 
   async saveSingleMeetLink(link: MeetLink, env?: "dev" | "prod") {
     const defaultMeetLinksLocation =
-      env === "dev" ? `/${env}/meetLinks.json` : `/meetLinks.json`;
-    const meetLinks: MeetLink[] = await this.get(defaultMeetLinksLocation);
+      env === "dev" ? `/${env}/meetLinks` : `/meetLinks`;
+    const meetLinks: MeetLink[] = await this.get(
+      defaultMeetLinksLocation + ".json"
+    );
     const insertAtIndex = getIndexToModify(
       link,
       meetLinks,
       (i, a) => i.id === a.id
     );
     return this.put(
-      `/${env}/meetLinks/${insertAtIndex.toString()}.json`,
+      `${defaultMeetLinksLocation}/${insertAtIndex.toString()}.json`,
       JSON.stringify(link)
     );
   }
