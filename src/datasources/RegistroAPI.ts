85a4289f7113d793532bbf858b69862195ed9082
Gonzalo Rocha <grochadc@gmail.com>
2021-08-18 08:47:54 -0500

    change registroAPI to use this.context.enviroment instead of passing env

--- src/datasources/RegistroAPI.ts
+++ src/datasources/RegistroAPI.ts
@@ -10,22 +10,19 @@ class RegistroAPI extends RESTDataSource {
     this.baseURL = "https://filex-5726c.firebaseio.com/registro";
   }
 
-  async getLevelsRegistering(
-    course: Course,
-    env: ClientEnv = "prod"
-  ): Promise<LevelsRegistering> {
-    const localUrl = `${env}/registeringLevels`;
+  async getLevelsRegistering(course: Course): Promise<LevelsRegistering> {
+    const localUrl = `${
+      this.context.enviroment ? this.context.enviroment : "prod"
+    }/registeringLevels`;
     const levels = await this.get(`${localUrl}/${course}.json`);
     if (levels === null) return [];
     return levels;
   }
 
-  async getApplicant(codigo: string, env?: ClientEnv): Promise<Applicant> {
-    const localUrl = env;
+  async getApplicant(codigo: string): Promise<Applicant> {
+    const localUrl = this.context.enviroment;
     const APPLICANT_NOT_FOUND = "APPLICANT_NOT_FOUND";
     //alreadyRegistered: {"12345678900": "E3-5"}
-    const registeredGroup = await this.getAlreadyRegistered(codigo, env);
-    console.log("registeredGroup", registeredGroup);
     const applicant: Applicant = await this.get(
       `${localUrl}/applicants/${codigo}.json`
     );
@@ -40,10 +37,9 @@ class RegistroAPI extends RESTDataSource {
   async getSchedule(
     level: string,
     group: string,
-    course: Course,
-    env: ClientEnv = "prod"
+    course: Course
   ): Promise<Schedule> {
-    const localUrl = env;
+    const localUrl = this.context.enviroment;
     const schedule = await this.get(
       `${localUrl}/schedules/${course}/level${level}/${group}.json`
     );
@@ -54,28 +50,21 @@ class RegistroAPI extends RESTDataSource {
     return schedule;
   }
 
-  async registerStudent(
-    student: Student,
-    course: Course,
-    env: ClientEnv = "prod"
-  ) {
-    const localUrl = env;
+  async registerStudent(student: Student, course: Course) {
+    const localUrl = this.context.enviroment;
     //runtime checks
     if (student.grupo === undefined) throw new Error("Group was not provided.");
     if (student.nivel === undefined) throw new Error("Level was not provided.");
     if (student.curso === undefined) throw new Error("Course was not provided");
 
-    const registeredGroup = await this.getAlreadyRegistered(
-      student.codigo,
-      env
-    );
+    const registeredGroup = await this.getAlreadyRegistered(student.codigo);
     if (registeredGroup)
       throw new ApolloError(`${registeredGroup}`, ALREADY_REGISTERED);
     this.post(
       `${localUrl}/availableGroups/${student.curso}/${student.nivel}/${student.grupo}.json`,
       "1"
     ).catch(e => console.log("this.post Error", e));
-    this.setAlreadyRegistered(student.codigo, student.grupo, env);
+    this.setAlreadyRegistered(student.codigo, student.grupo);
 
     const parsedStudent = [
       student.codigo,
@@ -101,20 +90,15 @@ class RegistroAPI extends RESTDataSource {
     const schedule = await this.getSchedule(
       student.nivel,
       student.grupo,
-      course,
-      env
+      course
     );
     const composedStudent: RegisterResponse = { ...student, schedule };
 
     return composedStudent;
   }
 
-  async setLevelsRegistering(
-    levels: Level[],
-    course: Course,
-    env: ClientEnv = "prod"
-  ) {
-    const localUrl = env;
+  async setLevelsRegistering(levels: Level[], course: Course) {
+    const localUrl = this.context.enviroment;
     this.put(`${localUrl}/registeringLevels/${course}.json`, levels);
     return levels;
   }
@@ -138,7 +122,7 @@ class RegistroAPI extends RESTDataSource {
     }
     */
     const groupsCounter = await this.get(
-      `system/availableGroups/${course}/level${level}.json`
+      `${this.context.enviroment}/availableGroups/${course}/level${level}.json`
     );
 
     if (groupsCounter === null) return [];
@@ -150,9 +134,9 @@ class RegistroAPI extends RESTDataSource {
     );
   }
 
-  async getSchedules(level: string, course: Course, env: ClientEnv = "prod") {
+  async getSchedules(level: string, course: Course) {
     const schedulesObj = await this.get(
-      `${env}/schedules/${course}/level${level}.json`
+      `${this.context.enviroment}/schedules/${course}/level${level}.json`
     );
     if (schedulesObj === null)
       throw new Error(`There are no schedules in /${course}/level${level}`);
@@ -164,15 +148,14 @@ class RegistroAPI extends RESTDataSource {
     this.get(`students/${course}/level${level}.json`);
   }
 
-  getAlreadyRegistered(
-    codigo: string,
-    env: ClientEnv = "prod"
-  ): Promise<string | null> {
-    return this.get(`${env}/alreadyRegistered/${codigo}.json`);
+  getAlreadyRegistered(codigo: string): Promise<string | null> {
+    return this.get(
+      `${this.context.enviroment}/alreadyRegistered/${codigo}.json`
+    );
   }
-  setAlreadyRegistered(codigo: string, grupo: string, env: ClientEnv = "prod") {
+  setAlreadyRegistered(codigo: string, grupo: string) {
     this.put(
-      `${env}/alreadyRegistered/${codigo}.json`,
+      `${this.context.enviroment}/alreadyRegistered/${codigo}.json`,
       JSON.stringify(grupo)
     ).catch(e => console.log("Put already registered", e.extensions.response));
   }
