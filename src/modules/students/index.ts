f578d0d1d7a187bdce29f68d3b8610f67765c6c9
Gonzalo Rocha <grochadc@gmail.com>
2022-09-10 09:49:20 -0500

    fix merging issues with master

--- src/modules/students/index.ts
+++ src/modules/students/index.ts
@@ -4,6 +4,7 @@ import { Resolvers } from "../../generated/graphql";
 export const typeDefs = gql`
   extend type Query {
     student(codigo: ID!): Student!
+    masterlist(ciclo: String!): [Student!]!
   }
 
   type Student {
@@ -24,25 +25,24 @@ export const typeDefs = gql`
   }
 
   extend type Mutation {
-    addStudent(student: StudentInput!): Student!
     editStudent(codigo: ID!, changes: StudentChangesInput): Student!
   }
 
   input StudentInput {
     codigo: ID!
-    nombre: String!
-    apellido_materno: String!
-    apellido_paterno: String!
-    genero: String!
-    carrera: String!
+    nombre: String
+    apellido_materno: String
+    apellido_paterno: String
+    genero: String
+    carrera: String
     ciclo: String!
-    telefono: String!
-    email: String!
-    nivel: String!
-    grupo: String!
-    externo: Boolean!
+    telefono: String
+    email: String
+    nivel: Int
+    grupo: String
+    externo: Boolean
     institucionalEmail: String
-    curso: String!
+    curso: String
   }
 
   input StudentChangesInput {
